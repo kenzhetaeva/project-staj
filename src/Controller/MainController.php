45b7fb97c3f6eb0d64bc4ab699ed0619a16fa68f
kenzhetaeva <kenzhetaeva11@gmail.com>
2020-03-14 20:35:47 +0600

    Проверка в использовании введеного логина в регистрации

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,7 +3,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Accounts;
 
 
 class MainController extends AbstractController{
@@ -48,6 +51,29 @@ class MainController extends AbstractController{
 	  public function signIn() {
 		return $this->render('staj-site/sign_in.html.twig', []);
 	}
+
+	/**
+     * @Route("/search_user_name", name="searchUserName")
+     */
+    public function searchUserName(Request $request) {
+		$user_name = $request->request->get('user_name');	
+		$repository = $this->getDoctrine()->getRepository(Accounts::class);
+		$accounts = $repository->searchUserName($user_name);
+		if($accounts) 
+			return $this->render('staj-site/error_register.html.twig', []);
+		
+		// return $this->render('staj-site/success_register.html.twig', ["accounts" => $accounts]);
+    }
+
+	/**
+     * @Route("/search_account", name="searchAccount")
+     */
+    public function searchAccount(Request $request) {
+		$user_name = $request->request->get('user_name');
+		$repository = $this->getDoctrine()->getRepository(Accounts::class);
+		$accounts = $repository->searchUserName($user_name);
+		// return $this->render('staj-site/error_register.html.twig', ["accounts" => $accounts]);
+    }
 }
 
 ?>
